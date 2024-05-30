b23ee8a592748d155835fc6db7601a9a05667671
mahesh <maheshkuraganti9@gmail.com>
2024-05-30 13:53:59 +0530

    us east 1 delete and commited

--- versionsmanifest/version.tf
+++ versionsmanifest/version.tf
@@ -15,25 +15,11 @@ provider "aws" {
   profile = "default"
 }
 
-#provider Block for us-east-1
-provider "aws" {
-  region = "us-east-1"
-  profile = "default"
-  alias = "aws-us-east-1"
-}
+
 
 #Provider Block for us-west-1
 provider "aws" {
   region = "us-west-1"
   profile = "default"
   alias = "aws-us-west-1"
-}
-
-#Resource Block for us east 1 vpc
-resource "aws_vpc" "myuseast1vpc" {
-  cidr_block = "10.0.0.0/16"
-  provider = aws.aws-us-east-1
-  tags = {
-    "Name" = "myuseast1vpc"
-      }
 }
