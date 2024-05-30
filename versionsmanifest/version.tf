b4865b19a21fd43141f330f5c8ff9638382e61cc
mahesh <maheshkuraganti9@gmail.com>
2024-05-30 11:22:36 +0530

    edited and commited

--- versionsmanifest/version.tf
+++ versionsmanifest/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = "~>1.8.4"
   required_providers {
     aws = {
-        source = "haschicorp/aws"
+        source = "hashicorp/aws"
         version = "~>5.0"   
          }
   }
